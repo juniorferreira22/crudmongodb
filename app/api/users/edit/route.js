974ec3189912e484251ae8f3e911118b7dc328b6
Junior Ferreira <jr.ferreira0018@gmail.com>
2024-07-11 20:59:05 -0300

    crud ready

--- app/api/users/edit/route.js
+++ app/api/users/edit/route.js
@@ -13,3 +13,26 @@ export async function GET(req) {
         return NextResponse.json(users, { status: 200 });
 }
 
+export async function PUT(req, {params}) {
+    
+    try {
+        // pegando o id do usuário a ser atualizado no banco de dados
+        const id = req.nextUrl.searchParams.get('id');
+
+        // criando novas variaveis para substituir o usuario e email
+        const {NewUsername: username, NewUsermail: usermail} = await req.json();
+
+        // conectando ao banco de dados
+        await connectToDatabase();
+
+        // atualizando os dados do usuário no banco de dados
+        await User.findByIdAndUpdate(id, {username, usermail});
+
+        // retornando uma mensagem de sucesso ao atualizar os dados do usuário no banco de dados
+        return NextResponse.json({message:"User updated successfully"}, {status: 201});
+        
+    } catch (error) {
+        console.log(error);
+    }
+
+}
--- app/components/EditUserForm/EditUser.jsx
+++ app/components/EditUserForm/EditUser.jsx
@@ -1,28 +1,60 @@
-'use client'
+import { useRouter } from "next/navigation";
+import React, { useState, useEffect } from "react";
 
-const findUser = async (id) => {
-    try {
-        const res = await fetch(`http://localhost:3000/api/users/edit/?id=${id}`, { cache: "no-store" }, { method: "GET" });
-        const user = res.json();
-        if (!res.ok) {
-            throw new Error(`Usuário não encontrado: ${res.status}, ${res.statusText}`);
-        }
-
-        return user;
-
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-export default async function EditUserForm({ id }) {
+export default function EditUserForm({ id }) {
     
-    const id_local = id
-    const userdata = await findUser(id_local);
+    const [user, setUser] = useState({ username: "", usermail: "" });
+    const router = useRouter();
+    const [newName, setNewName] = useState();
+    const [newMail, setNewMail] = useState();
+
+    useEffect(() => {
+        // buscando o user pelo ID passado pelo params, dentro da API
+        const fetchUser = async () => {
+            try {
+                const res = await fetch(`http://localhost:3000/api/users/edit/?id=${id}`, { cache: "no-store" });
+                if (!res.ok) {
+                    throw new Error(`Usuário não encontrado: ${res.status}, ${res.statusText}`);
+                }
+                const userData = await res.json();
+                setUser(userData);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+
+        fetchUser();
+    }, [id]);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+
+            const res = await fetch(`http://localhost:3000/api/users/edit/?id=${id}`, {
+                method: "PUT",
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ NewUsername: newName, NewUsermail: newMail })
+            });
+
+            if (!res.ok) {
+                throw new Error(`Erro ao editar usuário: ${res.status}, ${res.statusText}`);
+            }
+
+
+            if (res.ok) {
+                alert('Usuário editado com sucesso!');
+                router.push('/');
+                router.refresh();
+            }
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
 
     return (
-
-        <form>
+        <form onSubmit={handleSubmit}>
             <div className="flex flex-col max-w-3xl m-auto gap-2 text-center mt-4 bg-slate-900 p-4 pb-12 rounded-lg">
                 <h1 className="mt-2 m-4">Editar usuário</h1>
 
@@ -30,16 +62,16 @@ export default async function EditUserForm({ id }) {
                     className="flex p-3 rounded-lg bg-slate-600"
                     type="text"
                     placeholder="Nome"
-                    onChange={(e) => SetNewName(e.target.value)}
-                    defaultValue={userdata.username}
+                    onChange={(e) => setNewName(e.target.value)}
+                    defaultValue={user.username}
                 />
 
                 <input
                     className="flex p-3 rounded-lg bg-slate-600"
                     type="email"
-                    placeholder="email"
-                    onChange={(e) => SetNewMail(e.target.value)}
-                    defaultValue={userdata.usermail}
+                    placeholder="Email"
+                    onChange={(e) => setNewMail(e.target.value)}
+                    defaultValue={user.usermail}
                 />
 
                 <button
